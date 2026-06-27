e1c58539bcbe61922e27444a6268b024471dd25e
fardan <fardan.wardhana@gmail.com>
2026-06-27 16:03:00 +0700

    initiate project

---
+++ Jeda/App/JedaApp.swift
@@ -0,0 +1,29 @@
+//
+//  JedaApp.swift
+//  Jeda
+//
+//  Created by Muhamad Fardan Wardhana on 27/06/26.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct JedaApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
